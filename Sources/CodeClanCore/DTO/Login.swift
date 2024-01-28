57c252fd710769c71947ca152d6da99b634907d5
MahdiHashemloo <mahdi@lyf.app>
2024-01-28 15:25:30 +0330

    user model updated

--- Sources/CodeClanCore/DTO/Login.swift
+++ Sources/CodeClanCore/DTO/Login.swift
@@ -14,18 +14,30 @@ public extension ServerModels.Response{
         public var idToken: String?
         public var refreshToken: String?
         public var user: UserModel?
+        
+        enum CodingKeys: String,CodingKeys {
+        case created,expiresIn,refreshToken,user
+        case idToken = "jwtToken"
+        }
     }
 
 
     struct UserModel: Codable{
+        public var id: String?
         public var displayName: String?
         public var email: String?
+        public var fullName: String?
+        public var phoneNumber: String?
         public var emailVerified: Bool?
         public var federatedId: String?
-        public var firstName: String?
-        public var lastName: String?
+        public var name: String?
+        public var surname: String?
+        public var role: String?
         public var localId: String?
         public var photoUrl: String?
+        public var creationDate: String?
+        public var isVerified: Bool?
+        public var Devices: Any?
     }
 }
 
